f6c5eba014d36821c5b7e31c814ff4faa1003a16
Reichelle R <rmnrobianes@gmail.com>
2022-09-25 13:06:26 -0700

    Display user input

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridCanvas = document.querySelector('#grid');
 const ctx = gridCanvas.getContext('2d');
 
+ctx.font = "40px courier";
+ctx.textBaseLine = "bottom";
+
 const size = 9;
 const boxSize = gridCanvas.width / size;
 
@@ -9,6 +12,8 @@ let col;
 
 let prevRow = -1;
 let prevCol = -1;
+let prevX;
+let prevY;
 
 let matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,7 +27,6 @@ let matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-
 function drawLineWidth(pos){
     if (pos%150 === 0)
         ctx.lineWidth = 3;
@@ -30,7 +34,7 @@ function drawLineWidth(pos){
         ctx.lineWidth = 1;
 }
 
-function drawGrid(canvas, ctx){
+function drawGridLines(canvas, ctx){
     for (let y = 0; y <= canvas.height; y+=boxSize){
         ctx.beginPath();
         ctx.moveTo(y, 0);
@@ -48,9 +52,80 @@ function drawGrid(canvas, ctx){
     }
 }
 
+function drawSelected(ctx, row, col){
+    let x = col * boxSize;
+    let y = row * boxSize;
+    let empty = true;
+
+    if (prevCol == -1 && prevRow == -1){
+        prevRow = row;
+        prevCol = col;
+        prevX = prevCol * boxSize;
+        prevY = prevRow * boxSize;
+    }
+
+    if (matrix[row][col] !== 0){
+        console.log("first")
+        ctx.fillStyle = "#b8e7f3";
+        ctx.fillRect(x+3, y+3, boxSize-7, boxSize-7);
+        drawNum(row, col, matrix[row][col]);
+        empty = false;
+        if (matrix[prevRow][prevCol] != 0)
+            drawNum(prevRow, prevCol, matrix[prevRow][prevCol]);
+    }
+
+    if (prevRow === row && prevCol === col && empty){
+        console.log("second")
+        ctx.fillStyle = "#b8e7f3";
+        ctx.fillRect(x+3, y+3, boxSize-7, boxSize-7);
+    }
+    else if (prevRow === row && prevCol === col && !empty){console.log("third"); return;}
+    
+
+    else{
+        console.log("fourth")
+        ctx.clearRect(prevX + 3, prevY + 3, boxSize - 7, boxSize -7);
+        if(empty){
+            ctx.fillStyle = "#b8e7f3";
+            ctx.fillRect(x+3, y+3, boxSize-7, boxSize-7);
+        }    
+        if (matrix[prevRow][prevCol] != 0)
+            drawNum(prevRow, prevCol, matrix[prevRow][prevCol], true);
+    }
+    
+    prevRow = row;
+    prevCol = col;
+    prevX = prevCol * boxSize;
+    prevY = prevRow * boxSize;
+    
+}
+
+function drawNum(row, col, num, prev=false){
+    let x = col * boxSize;
+    let y = row * boxSize;
+    if (num == 0){
+        ctx.clearRect(x + 3, y + 3, boxSize - 7, boxSize -7);
+        ctx.fillStyle = "#b8e7f3";
+        ctx.fillRect(x+3, y+3, boxSize-7, boxSize-7);
+        return;
+    }
+    else if (!prev){
+        ctx.fillStyle = "#b8e7f3";
+        ctx.fillRect(x+3, y+3, boxSize-7, boxSize-7);
+        ctx.fillStyle = "#4090a7";
+        ctx.fillText(num, x+15, y+40);
+    }
+    else{
+        ctx.fillStyle = "#4090a7";
+        ctx.fillText(num, x+15, y+40);
+    }
+}
+
 function handleInput(e){
     const key = Number(e.key);
     if (isNaN(key)) return;
+    if (matrix[row][col] == key) return;
+    drawNum(row, col, key);
     matrix[row][col] = key;
 }
 
@@ -59,8 +134,9 @@ gridCanvas.addEventListener("click", evt => {
     col = ~~(evt.offsetX/boxSize);
     row = ~~(evt.offsetY/boxSize);
 
+    drawSelected(ctx, row, col);
     console.log(row, col);
 })
 
 document.addEventListener('keypress', handleInput);
-drawGrid(gridCanvas, ctx);
+drawGridLines(gridCanvas, ctx);
